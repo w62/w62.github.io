14f1e7d078851268ba9b852c4f48318994cb5058
william <william@freepackets.net>
2022-08-10 09:40:08 +0800

    Update gf.sh to generate addtional two spaces at the end
    According to [here](https://bywordapp.com/markdown/guide.html), it is
    better to add additional two spaces at the end. The little script has
    been updated to cater for it.

--- _posts/gf.sh
+++ _posts/gf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 gen_file_header () {
-	echo "---" >> $1
-	echo "layout: post" >> $1
-	echo "title: $2" >> $1
-	echo "date: $(date '+%Y-%m-%d %H:%M:%S %z')" >> $1
-	echo "categories: post" >> $1
+	echo "---  " >> $1
+	echo "layout: post  " >> $1
+	echo "title: $2  " >> $1
+	echo "date: $(date '+%Y-%m-%d %H:%M:%S %z')  " >> $1
+	echo "categories: post  " >> $1
 	echo >> $1
-	echo "---" >> $1
+	echo "---  " >> $1
 }
 
 FILENAME=$(date '+%Y-%m-%d-%H-%M-%S').md
